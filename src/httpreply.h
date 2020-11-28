b85c56407733f6f562f6cb25df137611f843f114
Flavio Tordini <flavio.tordini@gmail.com>
2020-11-28 16:45:02 +0100

    Fluent interface

--- src/httpreply.h
+++ src/httpreply.h
@@ -16,6 +16,19 @@ public:
     virtual QByteArray header(const QByteArray &headerName) const;
     virtual QByteArray body() const = 0;
 
+    template <typename Functor> HttpReply &onData(Functor lambda) {
+        connect(this, &HttpReply::data, this, lambda);
+        return *this;
+    }
+    template <typename Functor> HttpReply &onError(Functor lambda) {
+        connect(this, &HttpReply::error, this, lambda);
+        return *this;
+    }
+    template <typename Functor> HttpReply &onFinished(Functor lambda) {
+        connect(this, &HttpReply::finished, this, lambda);
+        return *this;
+    }
+
 signals:
     void data(const QByteArray &bytes);
     void error(const QString &message);
